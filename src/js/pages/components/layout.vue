ca68f9efed66cb6f8739e49bff6417ac273cf904
xiek <xk285985285@qq.com>
2017-12-11 15:44:41 +0800

    新增layout.vue 完善项目结构

---
+++ src/js/pages/components/layout.vue
@@ -0,0 +1,28 @@
+/*!
+* layout
+* create: 2017-12-06
+* since: 0.0.1
+*/
+
+<template lang="pug">
+	.rootBox
+		slot
+</template>
+
+<script>
+module.exports = {
+}
+</script>
+
+<style lang="scss" scoped>
+.rootBox{
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	z-index: 2;
+	overflow-y: auto;
+	background-color: #383e4a;
+}
+</style>
