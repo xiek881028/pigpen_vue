eb8447f0ff4e004193ab0b62997bb2b8cee9e3f7
xiek <xk285985285@qq.com>
2018-09-05 14:32:51 +0800

    9-5: 优化init方法，加入release方法

--- publicFn.js
+++ publicFn.js
@@ -5,31 +5,55 @@
  * since: 0.0.1
  */
 'use strict';
-const fs = require('fs');
+const fs = require('fs-extra');
 const path = require('path');
 
 module.exports = {
-	//isArray
-	//数组判断
-	isArray : function(it){
+	// isArray
+	// 数组判断
+	isArray: function (it) {
 		return Object.prototype.toString.call(it) === '[object Array]';
 	},
 
-	//fileTree
-	//循环指定目录，输出目录内所有文件列表的数组
-	fileTree : function(file){
-		let	fileList	=	[];
-		function walk(file){
-			let dirlist		=	fs.readdirSync(file);
-			dirlist.forEach(function(item){
-				if(fs.statSync(file + path.sep + item).isDirectory()){
-					walk(file + path.sep + item);
-				}else{
-					fileList.push(path.resolve(file + path.sep + item));
+	// fileTree
+	// 循环指定目录，输出目录内所有文件列表的数组
+	// file 文件名或文件夹名
+	// loop 是否循环遍历所有子目录
+	fileTree: function (file, loop = true) {
+		let fileList = [];
+		function walk(file) {
+			//如果入参是文件，直接加入数组返回
+			if(!fs.statSync(file).isDirectory()){
+				fileList.push(file);
+				return;
+			}
+			let dirlist = fs.readdirSync(file);
+			dirlist.forEach(function (item) {
+				let itemPath = path.resolve(file, item);
+				if (fs.statSync(itemPath).isDirectory() && loop) {
+					walk(itemPath);
+				} else {
+					fileList.push(itemPath);
 				}
 			});
 		};
 		walk(file);
 		return fileList;
 	},
+
+	// 数组合并处理
+	// mode 合并方式 diff：差集 sum：合集
+	mathArr: function (arr1, arr2, mode = 'diff') {
+		let set = new Set(arr2);
+		let out = [];
+		arr1.map(item => {
+			if(mode == 'diff' && !set.has(item)){
+				out.push(item);
+			}
+			if(mode == 'sum' && set.has(item)){
+				out.push(item);
+			}
+		});
+		return out;
+	}
 };
--- shell.js
+++ shell.js
@@ -8,114 +8,216 @@
 'use strict';
 
 const path = require('path');
-const fs = require('fs');
-const _fs = require('fs.extra');
-const rmdir = require('rmdir');
+const fs = require('fs-extra');
 const dateFormat = require('cainfoharbor-utils/dateFormat');
 const publicFn = require('./publicFn.js');
 const argv = require('yargs').argv;
 
+/*
+	key-value形式
+	key: 包名，必须对应真实包名
+	name: 文件夹名,不填以key值建立文件夹 unrequired
+	path: 资源路径，不填默认node_modules unrequired
+	assets: 资源列表,可以为字符串或数组,内容可为文件或文件夹 required
+*/
 const assets = {
 	vue: { assets: { js: ['dist/vue.min.js', 'dist/vue.js'] } },
 	'vue-router': { assets: { js: ['dist/vue-router.min.js', 'dist/vue-router.js'] } },
 	// vuex: { assets: { js: 'dist/vuex.min.js' } },
 };
 
+/* release时需要忽略的dist中的文件或文件夹 */
+const shellignore = ['assets', 'images'];
+
 const today = dateFormat('yyyy-MM-dd', new Date());
 
-if (argv.h) {
-	console.log(``);
-	console.log(`查看帮助： npm run shell -- --h`);
-	console.log(`开发环境初始化： npm run shell -- --init`);
-	console.log(`添加页面模块： npm run shell -- --add --name <name>`);
-	console.log(`删除页面模块： npm run shell -- --del --name <name>`);
-	console.log(``);
-}
-
-if (argv.init) {
-	console.log(`开始导入 assets, 配置列表在shell.js顶部的assets变量`);
-	const distPath = path.resolve(__dirname, 'dist');
-	const assetsPath = path.resolve(distPath, 'assets');
-	if (!fs.existsSync(distPath)) {
-		fs.mkdirSync(distPath);
+(async () => {
+	if (argv.h) {
+		console.log(``);
+		console.log(`查看帮助： npm run shell -- --h`);
+		console.log(`开发环境初始化： npm run shell -- --init`);
+		console.log(`添加页面模块： npm run shell -- --add --name <name>`);
+		console.log(`删除页面模块： npm run shell -- --del --name <name>`);
+		console.log(``);
 	}
-	if (!fs.existsSync(assetsPath)) {
+
+	if (argv.init) {
+		console.log(`开始导入 assets, 配置列表在shell.js顶部的assets变量`);
+		const distPath = path.resolve(__dirname, 'dist');
+		const assetsPath = path.resolve(distPath, 'assets');
+
+		//dist不存在则创建
+		let exists = await fs.pathExists(distPath);
+		!exists && await fs.ensureDir(distPath);
+		//创建assets文件夹
+		fs.emptyDir(assetsPath);
 		addAssets();
-	} else {
-		rmdir(assetsPath, (err) => {
-			if (err) throw err;
-			addAssets();
-		});
-	};
 
-	function addAssets() {
-		fs.mkdirSync(assetsPath);
-		Object.keys(assets).map((item) => {
-			!fs.existsSync(path.resolve(assetsPath, item)) && fs.mkdirSync(path.resolve(assetsPath, item));
-			const ASSETS_PATH = assets[item].path || 'node_modules';
-			Object.keys(assets[item].assets).map((_item) => {
-				let _asset = assets[item].assets[_item];
-				if (publicFn.isArray(_asset)) {
-					fs.mkdirSync(path.resolve(assetsPath, item, _item));
-					_asset.map((__item) => {
-						let isDirectory = fs.statSync(path.resolve(__dirname, ASSETS_PATH, item, __item)).isDirectory();
-						_fs[isDirectory ? 'copyRecursive' : 'copy'](path.resolve(__dirname, ASSETS_PATH, item, __item), path.resolve(assetsPath, item, _item, isDirectory ? '' : path.parse(__item).base), (err) => {
-							if (err) throw err;
-							console.log(`完成导入 ${item}/${_item} -> ${__item}`);
+		async function addAssets() {
+			//循环资源列表
+			Object.keys(assets).map(async params => {
+				//设置资源文件夹名
+				let dirName = assets[params].name || params;
+				//创建资源文件夹
+				await fs.ensureDir(path.resolve(assetsPath, dirName));
+				//设定资源路径
+				const ASSETS_PATH = assets[params].path || 'node_modules';
+				//循环资源文件
+				Object.keys(assets[params].assets).map(async assetObject => {
+					//获取单个资源文件下的一个资源类型
+					let _asset = assets[params].assets[assetObject];
+					if (publicFn.isArray(_asset)) {
+						//根据资源类型建立文件夹
+						await fs.ensureDir(path.resolve(assetsPath, dirName, assetObject));
+						_asset.map(async item => {
+							//判断资源是文件还是文件夹来决定复制路径
+							let copyPath = fs.statSync(path.resolve(__dirname, ASSETS_PATH, params, item)).isDirectory() ? '' : path.parse(item).base;
+							//将资源从源路径复制到assets文件夹指定位置内
+							try {
+								await fs.copy(path.resolve(__dirname, ASSETS_PATH, params, item), path.resolve(assetsPath, dirName, assetObject, copyPath));
+								console.log(`完成导入 ${params}/${assetObject} -> ${item}`);
+							} catch (error) {
+								throw error;
+							}
 						});
-					});
-				} else if (typeof (_asset) == 'string') {
-					fs.mkdirSync(path.resolve(assetsPath, item, _item));
-					let isDirectory = fs.statSync(path.resolve(__dirname, ASSETS_PATH, item, _asset)).isDirectory();
-					_fs[isDirectory ? 'copyRecursive' : 'copy'](path.resolve(__dirname, ASSETS_PATH, item, _asset), path.resolve(assetsPath, item, _item, isDirectory ? '' : path.parse(_asset).base), (err) => {
-						if (err) throw err;
-						console.log(`完成导入 ${item}/${_item} -> ${_asset}`);
-					});
-				};
+					} else if (typeof (_asset) == 'string') {
+						//获取单个资源文件下的一个资源类型
+						await fs.ensureDir(path.resolve(assetsPath, dirName, assetObject));
+						//判断资源是文件还是文件夹来决定复制路径
+						let copyPath = fs.statSync(path.resolve(__dirname, ASSETS_PATH, params, _asset)).isDirectory() ? '' : path.parse(item).base;
+						//将资源从源路径复制到assets文件夹指定位置内
+						try {
+							await fs.copy(path.resolve(__dirname, ASSETS_PATH, params, _asset), path.resolve(assetsPath, dirName, assetObject, copyPath));
+							console.log(`完成导入 ${params}/${assetObject} -> ${_asset}`);
+						} catch (error) {
+							throw error;
+						}
+					};
+				});
 			});
-		});
-	};
-}
-
-if (argv.add) {
-	if (!argv.name || !argv.name.length) {
-		console.error(`错误: 页面名称不能为空!`);
-	} else {
-		argv.name.split(',').map((name) => {
-			let same = 0;
-			publicFn.fileTree(path.resolve(__dirname, 'src/html')).map((item) => {
-				if (name == path.parse(item).name) same = 1;
-				return;
-			});
-			if (same) {
-				console.error(`错误: ${name} 页面已存在!`);
-			} else {
-				fs.writeFile(path.resolve(__dirname, 'src/js/pages', `${name}.vue`),
-					`/*!\n* ${name}\n* create: ${today}\n* since: 0.0.1\n*/\n\n<template lang="pug">\n</template>\n\n<script>\n</script>\n\n<style lang="scss" scoped>\n</style>\n`,
-					(err) => {
-						if (err) throw err;
-						console.log(`${name}.vue 创建成功 `);
-					}
-				);
-			}
-		});
+		};
 	}
-}
 
-if (argv.del) {
-	if (!argv.name || !argv.name.length) {
-		console.error(`错误: 页面名称不能为空!`);
-	} else {
-		argv.name.split(',').map((name) => {
-			publicFn.fileTree(path.resolve(__dirname, 'src/js/pages')).map((item) => {
-				if (name == path.parse(item).name) {
-					fs.unlink(item, (err) => {
-						if (err) throw err;
-						console.log(`${path.parse(item).base} 删除成功`);
-					});
+	if (argv.add) {
+		if (!argv.name || !argv.name.length) {
+			console.error(`错误: 页面名称不能为空!`);
+		} else {
+			argv.name.split(',').map(async name => {
+				let same = 0;
+				publicFn.fileTree(path.resolve(__dirname, 'src/html')).map(async item => {
+					if (name == path.parse(item).name) same = 1;
 					return;
+				});
+				if (same) {
+					console.error(`错误: ${name} 页面已存在!`);
+				} else {
+					try {
+						await fs.outputFile(path.resolve(__dirname, 'src/js/pages', `${name}.vue`),
+							`/*!
+* ${name}
+* create: ${today}
+* since: 0.0.1
+*/
+
+<template lang="pug">
+</template>
+
+<script>
+export default {
+
+	components: {},
+
+	props: {},
+
+	data() {
+		return {};
+	},
+
+	computed: {},
+
+	watch: {},
+
+	methods: {},
+
+	mounted() {},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>`
+						);
+						console.log(`${name}.vue 创建成功 `);
+					} catch (error) {
+						throw error;
+					}
 				}
 			});
+		}
+	}
+
+	if (argv.del) {
+		if (!argv.name || !argv.name.length) {
+			console.error(`错误: 页面名称不能为空!`);
+		} else {
+			argv.name.split(',').map(async name => {
+				publicFn.fileTree(path.resolve(__dirname, 'src/js/pages')).map(async item => {
+					if (name == path.parse(item).name) {
+						try {
+							fs.remove(item);
+							console.log(`${path.parse(item).base} 删除成功`);
+						} catch (error) {
+							throw error;
+						}
+						return;
+					}
+				});
+			});
+		}
+	}
+
+	if (argv.release) {
+		let delPath = [];
+		removeItem(shellignore, path.resolve(__dirname, 'dist'));
+
+		function removeItem(ignore, findPath){
+			// 当前层忽略的路径数组
+			let floorPathignore = [];
+			// 忽略的源文件对象
+			let ignoreObj = {};
+			// 当前层级深度
+			let basePathLen = findPath.split(path.sep).length;
+			// 循环忽略数组
+			ignore.map(item => {
+				let itemPath = path.resolve(findPath, item);
+				// 分割忽略文件路径
+				let itemPathArr = itemPath.split(path.sep);
+				if(itemPathArr[basePathLen] != undefined){
+					//忽略文件在当前层级的路径
+					let nowPathIgnore = path.resolve(findPath, itemPathArr[basePathLen]);
+					//当前层已经处理，不打入待处理对象
+					if(nowPathIgnore != itemPath){
+						//新建待处理对象值为数组
+						if(ignoreObj[nowPathIgnore] == undefined){
+							ignoreObj[nowPathIgnore] = [];
+						}
+						//将待处理对象打入
+						ignoreObj[nowPathIgnore].push(itemPath);
+					}
+					floorPathignore.push(nowPathIgnore);
+				}
+			});
+			// 需要进一步处理的文件路径数组
+			let sum = publicFn.mathArr(publicFn.fileTree(path.resolve(findPath), false), floorPathignore, 'sum');
+			// 一定会删除的文件路径数组
+			let diff = publicFn.mathArr(publicFn.fileTree(path.resolve(findPath), false), sum);
+			//循环需要进一步处理的文件对象
+			Object.keys(ignoreObj).map(item => {
+				removeItem(ignoreObj[item], item);
+			});
+			delPath.push(...diff);
+		}
+		delPath.map(item => {
+			fs.remove(item);
 		});
 	}
-}
+})();
